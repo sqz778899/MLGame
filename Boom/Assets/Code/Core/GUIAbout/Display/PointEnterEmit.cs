7ca2384e2c080ab49a4125398a1dbe90215a1e62
sqz778899 <18210142576@163.com>
2024-07-05 19:07:04 +0800

    ssssssssssssssss

---
+++ Boom/Assets/Code/Core/GUIAbout/Display/PointEnterEmit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class PointEnterEmit:MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Boom/Assets/Code/Core/GUIAbout/Display/PointEnterMove.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PointEnterMove:MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public float AniTime = 0.5f;
+    public AnimationCurve Curve;
+    public Transform TargetTrans;
+    Vector3 OriginPos;
+
+    void Start()
+    {
+        OriginPos = transform.position;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOMove(TargetTrans.position, AniTime).SetEase(Curve);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOMove(OriginPos, AniTime).SetEase(Curve);
+    }
+}
