ddf34a3c5f415cff16763435ab386dfe04e9e789
sqz778899 <18210142576@163.com>
2025-02-23 21:22:38 +0800

    pppppppppppppppppppp

--- Boom/Assets/Code/Core/GUIAbout/FindRoot/GUIMap.cs
+++ Boom/Assets/Code/Core/GUIAbout/FindRoot/GUIMap.cs
@@ -6,8 +6,6 @@ using UnityEngine.Serialization;
 public class MapScene : MonoBehaviour
 {
     public GameObject MapLogicGO;
-    public GameObject MapNode;
     public GameObject ShopRoot;
-    public GameObject REventRoot;
     public GameObject RewardRoot;
 }
